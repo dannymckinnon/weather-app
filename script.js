87f0b73ecf98891f70d09981049362cda54b1771
Danny McKinnon <dannymckinnon.dm@gmail.com>
2023-02-19 16:32:19 -0800

    Add json parsing of data to getWeatherData function

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-async function getWeatherData(location) {
-  const result = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=700e25afd76eac4e4a2c6a616b26ba3b`,
+async function getWeatherData(location, units = 'metric') {
+  const data = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=${units}&APPID=700e25afd76eac4e4a2c6a616b26ba3b`,
     { mode: 'cors' }
   );
-  console.log(result);
+  const jsonData = await data.json();
+  return jsonData;
 }
 
-getWeatherData('vancouver');
+getWeatherData('vancouver').then((data) => console.log(data));
