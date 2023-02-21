8325f4c01ea61d61d1ef0caef9f587c93aa6fd84
Danny McKinnon <dannymckinnon.dm@gmail.com>
2023-02-20 18:27:24 -0800

    Put getWeatherData function into its own module

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-async function getWeatherData(location, units = 'metric') {
-  const data = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=${units}&APPID=700e25afd76eac4e4a2c6a616b26ba3b`,
-    { mode: 'cors' }
-  );
-  const jsonData = await data.json();
-  return jsonData;
-}
+import getWeatherData from './weatherdata.js';
 
 // do another function that gets windspeed in mph by calling with units as
 // imperial regardless of the units your fetching with
---
+++ weatherdata.js
@@ -0,0 +1,10 @@
+async function getWeatherData(location, units = 'metric') {
+  const data = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=${units}&APPID=700e25afd76eac4e4a2c6a616b26ba3b`,
+    { mode: 'cors' }
+  );
+  const jsonData = await data.json();
+  return jsonData;
+}
+
+export default getWeatherData;
