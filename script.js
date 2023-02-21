e87712fc512c4a0b0ee85e34dda9f07a9e937742
Danny McKinnon <dannymckinnon.dm@gmail.com>
2023-02-20 18:37:05 -0800

    Add event listiner for enter key press

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-import getWeatherData from './weatherdata.js';
+import getWeatherData from './weather-data.js';
+
+const input = document.querySelector('.city-input input');
+
+input.addEventListener('keydown', (e) => {
+  if (e.keyCode === 13) {
+    console.log(input.value);
+    input.value = '';
+  }
+});
 
 // do another function that gets windspeed in mph by calling with units as
 // imperial regardless of the units your fetching with
